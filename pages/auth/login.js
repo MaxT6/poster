372f180dea3b7e197c945b97c3da140da38c6c30
MaxT6 <maximiliant6@gmail.com>
2022-11-08 14:28:34 -0500

    Created login page and used firebase for auth. Added routes and installed npm react-firebase-hooks

---
+++ pages/auth/login.js
@@ -0,0 +1,31 @@
+import { FcGoogle } from "react-icons/fc";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../utilities/firebase.js";
+import { useRouter } from "next/router";
+
+const Login = () => {
+  const route = useRouter();
+  // Sign in with Google
+  const googleProvider = new GoogleAuthProvider();
+  const GoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      route.push("/;")
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
+      <h2 className="text-2xl font-medium">JOIN TODAY</h2>
+      <div className="py-4"> {/* adds a space */}
+        <h3 className="py-4">Sign in with one of the providers</h3>
+        <button onClick={GoogleLogin} className="text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2"> <FcGoogle className="text-2xl" /> Sign in with Google</button>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Login
