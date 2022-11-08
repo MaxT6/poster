c057f85101370196433014f0c91e2966d8f5627c
MaxT6 <maximiliant6@gmail.com>
2022-11-08 12:26:24 -0500

    updated nav bar with join now button and return home link

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,18 @@
 
+import Link from "next/link";
+
 const Nav= () => {
   return (
-    <div>
-      <h1>NAV PLACEHOLDER</h1>
-    </div>
+    <nav className="flex justify-between items-center py-10">
+      <Link href="/">
+        <button className="text-lg font-medium">Posters</button>
+      </Link>
+      <ul className="flex items-center gap-10">
+        <Link href={"/auth/login"}>
+          <button className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">Join Now</button>
+        </Link>
+      </ul>
+    </nav>
   );
 }
 
